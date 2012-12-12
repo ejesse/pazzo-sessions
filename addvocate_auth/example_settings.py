ff8435500ea020e8383cd6546c119ae31fc06412
Jesse Emery <jesse@addvocate.com>
2012-12-11 19:50:59 -0800

    Adds example settings

---
+++ addvocate_auth/example_settings.py
@@ -0,0 +1,29 @@
+""" General cookie settings """
+
+SESSION_COOKIE_AGE = 1209600 # Cookie age in seconds, 1209600 == two weeks 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+SESSION_COOKIE_NAME = 'sessionid'
+SESSION_COOKIE_DOMAIN = '127.0.0.1'
+SESSION_COOKIE_PATH = '/'
+SESSION_COOKIE_SECURE = None
+SESSION_COOKIE_HTTPONLY = None
+SESSION_WSGI_ENVIRON_NAME = 'addvocate_session'
+SESSION_KEY_SALT = "addvocate_auth.sessions.base.BaseSession"
+
+"""
+Generate a secret key (You can use different number than 64):
+
+from addvocate_auth.utils import get_secret_string
+print get_secret_string(64)
+
+Copy that output to be the value of the SECRET_SESSION_KEY
+
+"""
+SECRET_SESSION_KEY = 'secret'
+
+""" Persistence settings"""
+ADDVOCATE_SESSION_STORE = 'addvocate_auth.sessions.stores.redis_store'
+REDIS_POOL_MAX_CONNECTIONS = 20
+REDIS_SESSIONS_HOST = 'localhost'
+REDIS_SESSIONS_PORT = 6379
+REDIS_SESSIONS_DB = 15
