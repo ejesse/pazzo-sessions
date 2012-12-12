48216206577bc17c9ab8e85d2bd6e71c09212cc9
Jesse Emery <jesse@addvocate.com>
2012-12-12 15:37:59 -0800

    Fixes default settings working with Django settings

--- pazzo/sessions/django_session.py
+++ pazzo/sessions/django_session.py
@@ -4,10 +4,14 @@ from pazzo.sessions.stores.store_registry import StoreRegistry
 from pazzo.exceptions import PazzoException
 
 try:
-    from django.conf import settings
+    from django.conf import settings as django_settings
 except ImportError:
     raise PazzoException("Django SessionStore requires Django")
 
+class FauxSettings(object):
+    pass
+
+    
 
 class SessionStore(BaseSession):
     
@@ -19,10 +23,13 @@ class SessionStore(BaseSession):
     def __init__(self, session_key=None):
         """ Sneakily set the settings from the Django settings
         so we can fool Django into initializing the registry
-        and still behave like both an pazzo session
+        and still behave like both a pazzo session
         and a Django SessionStore 
         """
         registry = StoreRegistry()
         if not registry.initialized:
+            settings = FauxSettings()
+            ## silly django....
+            settings.__dict__ = django_settings.__dict__['_wrapped'].__dict__
             StoreRegistry(settings=settings)
         super(SessionStore, self ).__init__(session_key=session_key)
